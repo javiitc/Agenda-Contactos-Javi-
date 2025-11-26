13ddba2ccea84645151a04cc050b878451538e36
Javi <itsjavii1610@gmail.com>
2025-11-26 11:57:13 +0100

    Arrays

--- Agenda contactos Javi/src/Main.java
+++ Agenda contactos Javi/src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner sc = new Scanner(System.in);
-
+        String [] contactos = new String[100];
+        int [] numeroContactos = new int[100];
+        int posicionContacto = 0;
         boolean salir = true;
         int seleccion;
         int seleccion2;
@@ -12,6 +14,8 @@ public class Main {
             System.out.println("Elija la opción que desea realizar");
             System.out.println("1. Contactos");
             System.out.println("2. Nuevo contacto");
+            System.out.println("3. Lista de contactos");
+            System.out.println("4. Modificar contacto");
             System.out.println("3. Salir del menú");
 
             seleccion = sc.nextInt();
@@ -27,14 +31,22 @@ public class Main {
 
                 case 2:
                     System.out.println("Añade nombre y numero de telefono");
-                    String nombre = sc.next();
-                    int numTelefono = sc.nextInt();
+                    contactos[posicionContacto] = sc.next();
+                    numeroContactos[posicionContacto] = sc.nextInt();
                     System.out.println("Nuevo contacto guardado");
-                    System.out.println(nombre);
-                    System.out.println("Numero: " + numTelefono);
+                    System.out.println(contactos[posicionContacto]);
+                    System.out.println("Numero: " + numeroContactos[posicionContacto]);
+                    posicionContacto++;
                     break;
 
                 case 3:
+                    System.out.println();
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
                     break;
             }
             System.out.println("Realizar otra operacion ?");
