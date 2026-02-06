562ca49c2d0ba36657afc93479713f90ab04c060
Javi <itsjavii1610@gmail.com>
2026-02-06 10:52:07 +0100

    metodo 2

--- Agenda contactos Javi/src/Metodos.java
+++ Agenda contactos Javi/src/Metodos.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Metodos {
 
-    public void mostrarContactos (String contactos[]) {
+    public void mostrarContactos (String contactos[], int numeroContactos []) {
         for (int i = 0; i < contactos.length; i++) {
             if (contactos[0] == null) {
                 System.out.println("No tienes contactos guardados, añade minimo uno para mostrar los contactos");
@@ -13,5 +15,18 @@ public class Metodos {
         }
     }
 
-    
+    public void agregarContacto (Scanner sc, String contactos [], int posicionContacto, int numeroContactos []) {
+        System.out.println("Añade nombre y numero de telefono");
+        System.out.println("-----------------------------------");
+        contactos[posicionContacto] = sc.next();
+        numeroContactos[posicionContacto] = sc.nextInt();
+        System.out.println("Nuevo contacto guardado");
+        System.out.println(contactos[posicionContacto]);
+        System.out.println("Numero: " + numeroContactos[posicionContacto]);
+        posicionContacto++;
+    }
+
+    public void editarContacto () {
+
+    }
 }
