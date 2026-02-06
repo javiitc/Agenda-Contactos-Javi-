da817b2109ce18778e3bfcf6720a55de26a15e55
Javi <itsjavii1610@gmail.com>
2026-02-06 11:46:54 +0100

    pro tip? no programar con resaca

--- Agenda contactos Javi/src/Main.java
+++ Agenda contactos Javi/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner sc = new Scanner(System.in);
+        Metodos metodo = new Metodos();
         String [] contactos = new String[100];
         int [] numeroContactos = new int[100];
         int posicionContacto = 0;
@@ -23,67 +24,21 @@ public class Main {
 
             switch (seleccion) {
                 case 1:
-
-                    for (int i = 0; i < contactos.length; i++) {
-                        if (contactos[0] == null) {
-                            System.out.println("No tienes contactos guardados, añade minimo uno para mostrar los contactos");
-                            break;
-                        } else if (contactos[i] == null) {
-                            break;
-                        } else {
-                            System.out.println((i + 1) + "." + contactos[i] + ": " + numeroContactos[i]);
-                        }
-                    }
+                    metodo.mostrarContactos();
                     break;
 
                 case 2:
-
-                    System.out.println("Añade nombre y numero de telefono");
-                    System.out.println("-----------------------------------");
-                    contactos[posicionContacto] = sc.next();
-                    numeroContactos[posicionContacto] = sc.nextInt();
-                    System.out.println("Nuevo contacto guardado");
-                    System.out.println(contactos[posicionContacto]);
-                    System.out.println("Numero: " + numeroContactos[posicionContacto]);
-                    posicionContacto++;
+                    metodo.agregarContacto();
                     break;
 
                 case 3:
-                    System.out.println("Selecciona el contacto que quieres editar");
-                    System.out.println("------------------------------------------");
-                    for (int i = 0; i < contactos.length; i++) {
-                        if (contactos[0] == null) {
-                            System.out.println("No tienes contactos guardados, añade minimo uno para mostrar los contactos");
-                            break;
-                        } else if (contactos[i] == null) {
-                            break;
-                        } else {
-                            System.out.println((i + 1) + "." + contactos[i] + ": " + numeroContactos [i]);
-                        }
-
-                    }
-
-                    seleccionContacto = sc.nextInt();
-
-                    System.out.println("Que deseas modificar de " + contactos[seleccionContacto - 1] + "?");
-                    System.out.println("1. Nombre");
-                    System.out.println("2. Numero de Telefono");
-                    modificar = sc.nextInt();
-                    switch (modificar) {
-                        case 1:
-
-                            System.out.println("Introduce el nuevo nombre para guardar el contacto: ");
-                            contactos [seleccionContacto - 1] = sc.next();
-                            break;
-                        case 2:
-
-                            System.out.println("Introduce el nuevo numero de telefono: ");
-                            numeroContactos [seleccionContacto -1 ] = sc.nextInt();
-                            break;
-                }
+                    metodo.editarContacto();
                     break;
 
                 case 4:
+                    metodo.buscarContacto();
+                    break;
+                case 5:
                     System.exit(0);
                     break;
             }
